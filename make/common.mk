ed49e9682d004516033ea5f36bd65606d5fd2658
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-06-16 19:52:34 +0200

    changes

--- make/common.mk
+++ make/common.mk
@@ -4,7 +4,7 @@ IDF := idf.py
 PORT := /dev/ttyUSB0
 ESP_IDF_TARGET ?= esp32s3
 
-PROJECT_NAME ?= devcontainer-esp-idf-clean
+PROJECT_NAME ?= project
 
 PROJECT_DIR := $(shell pwd)
 MAIN_DIR := $(PROJECT_DIR)/main
